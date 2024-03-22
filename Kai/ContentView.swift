cd3b8be5acff5f534a9f905a7d7b8112a1781e66
SheharaJ <sheharajaya7@gmail.com>
2024-03-22 23:12:07 +0530

    Initial Commit

---
+++ Kai/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Kai
+//
+//  Created by Shehara Jayasooriya on 2024-03-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Kai/KaiApp.swift
@@ -0,0 +1,17 @@
+//
+//  KaiApp.swift
+//  Kai
+//
+//  Created by Shehara Jayasooriya on 2024-03-22.
+//
+
+import SwiftUI
+
+@main
+struct KaiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
